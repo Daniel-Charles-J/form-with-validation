5d34df7cbf5d08a7aef2a37e123dd4bbb015e291
Daniel-Charles-J <80055586+Daniel-Charles-J@users.noreply.github.com>
2022-05-08 12:39:01 +0530

    error message

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ let email=document.getElementById('email');
 let password= document.getElementById('password');
 let confirmPassword=document.getElementById('confirmPassword');
 
+const inputArray = [userName,email,password,confirmPassword];
+
 //show error
 const showError=function(input,message){
         let formElement=input.parentElement;
-        formElement.classList="error";
+        formElement.classList='form-control error';
         let small=document.querySelector('small');
         small.innerText=message;
 
@@ -17,13 +19,19 @@ const showSuccess=function(input){
     let formElement=input.parentElement;
         formElement.classList='form-control success';
 }
+const checkRequired=function(inputArray){
+    inputArray.forEach((input)=>{
+        console.log(input);
+        if(input.value.trim()===''){
+            showError(input,`${input.name} is Required`);
+        }else{
+            showSuccess(input);
+        }
+    });
+};
 
 form.addEventListener('submit',function(event){
     console.log("submited");
     event.preventDefault();
-    if(userName.value.trim()==''){
-        showError(userName,'User name is required');
-    }else{
-       showSuccess(userName); 
-    }
+    checkRequired(inputArray);
 });
